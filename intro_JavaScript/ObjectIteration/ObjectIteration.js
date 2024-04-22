82209d49ff422d53a4b22c77985352f9c709c518
amanuelgthn <amanuelgthn@gmail.com>
2024-04-22 23:29:00 +0200

    Obect iteration

---
+++ intro_JavaScript/ObjectIteration/ObjectIteration.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
+
+function logDairy () {
+    for (item in dairy) {
+        console.log (dairy[item]);
+    }
+}
+
+const animal = {
+    canJump: true,
+}
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan () {
+    for (var item in bird) {
+        if (bird.hasOwnProperty(item)) {
+            console.log(`${item}: ${bird[item]}`);
+        }
+    }
+}
+function animalCan () {
+    for (var item in bird) {
+        console.log(`${item}: ${bird[item]}`);
+    }
+}
+logDairy();
+birdCan();
+animalCan();
